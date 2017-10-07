9420d9124244310d9f3a5cb325c4cfec0beecdcd
Roman <exculibur@rambler.ru>
2017-10-07 22:05:02 +0300

    Create pop.cpp

---
+++ examples/pop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Stack.hpp"
+
+using namespace std;
+
+void pop_ex() {
+	try {
+		cout << endl << "POP" << endl;
+		Stack<double> Double_arr;
+		for (int i = 0; i < 8; i++) { Double_arr.push(double(i)); }
+
+		cout << "RESULT ";
+
+		for (int j = 0; j < 4; j++) { cout << Double_arr.pop() << " "; }
+		cout << endl << endl;
+
+	}
+	catch (const std::exception& e) {
+		cout << e.what() << endl;
+	}
+}
