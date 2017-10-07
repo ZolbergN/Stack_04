410a407a2beffc8752a13b281e8f157700c64f3c
Roman <exculibur@rambler.ru>
2017-10-07 21:58:48 +0300

    Create Stack.hpp

---
+++ include/Stack.hpp
@@ -0,0 +1,104 @@
+#include <iostream> 
+#include <string> 
+#include <stdexcept> 
+
+using namespace std;
+
+template <typename T>
+class Stack {
+private:
+	T* array_;
+	size_t array_size_;
+	size_t count_;
+public:
+	Stack() noexcept : Stack(8) {}
+	Stack(size_t max_el) noexcept : array_size_(max_el), array_(new T[max_el]), count_(0) {}
+
+	Stack(const Stack& s) noexcept {
+		if (this != &s) {
+			delete[] array_;
+			array_size_ = s.array_size_;
+			count_ = s.count_;
+			array_ = new T[s.array_size_];
+			for (size_t i = 0; i < s.array_size_; ++i) {
+				array_[i] = s.array_[i];
+			}
+		}
+	}
+
+	Stack(Stack&& s) noexcept : array_size_(s.array_size_), count_(s.count_), array_(s.array_) {
+
+		s.array_size_ = 0;
+		s.count_ = 0;
+		s.array_ = nullptr;
+	}
+
+	auto operator=(const Stack& s) noexcept {
+		if (this == &s)
+			return *this;
+		else
+		{
+			delete[] array_;
+			array_size_ = s.array_size_;
+			count_ = s.count_;
+			array_ = new T[s.array_size_];
+			for (size_t i = 0; i < s.array_size_; ++i)
+			{
+				array_[i] = s.array_[i];
+			}
+			return *this;
+		}
+	}
+
+	auto operator=(const Stack&& s)
+	{
+		if (this == &s)
+			return *this;
+
+		array_size_ = s.array_size_;
+		array_ = s.array_;
+		s.array_ = nullptr;
+
+		return *this;
+	}
+
+	size_t count() const noexcept {
+		return count_;
+	}
+
+	bool empty() const noexcept {
+		if (count() == 0) return true;
+		return false;
+	}
+
+	void push(T const &value) noexcept {
+		if (count_ == array_size_) {
+			T* array_size2_ = new T[2 * array_size_];
+			for (int i = 0; i != array_size_; ++i) {
+				array_size2_[i] = array_[i];
+			}
+			array_size_ *= 2;
+			delete[] array_;
+			array_ = array_size2_;
+		}
+		array_[count_++] = value;
+	}
+
+	T& top() const noexcept {
+		return array_[count_];
+	}
+
+	T& pop() noexcept {
+		if (count_ == 0) domain_error{ "ERROR —- STACK EMPTY" };
+	--count_;
+		return top();
+	}
+
+	~Stack() noexcept {
+		delete[] array_;
+	}
+
+	T& operator[](int ij) noexcept {
+		return array_[ij];
+	}
+};
