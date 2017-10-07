339f69deea2fcfdbd0b2a5bbb32fe212a579bd48
Roman <exculibur@rambler.ru>
2017-10-07 21:59:30 +0300

    Create main.cpp

---
+++ sources/main.cpp
@@ -0,0 +1,28 @@
+int main() {
+	try {
+		Stack<int> st(10);
+
+		st.push(2);
+		st.push(4);
+		st.push(7);
+		st.push(5);
+		st.push(8);
+		st.push(766);
+		st.push(9);
+		st.push(32);
+		st.push(432);
+		st.push(1);
+
+		for (int i = 0; i < 10; i++) {
+			cout << st.pop() << endl;
+		}
+		cout << endl;
+
+	}
+	catch (const exception& e) {
+		cout << e.what() << endl;
+	}
+
+	system("pause");
+	return 0;
+}
