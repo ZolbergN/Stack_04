4cefaf7b08c61379188a96dfb2a529cdbcabf8f0
Roman <exculibur@rambler.ru>
2017-10-16 00:15:51 +0300

    Update Stack.hpp

--- include/Stack.hpp
+++ include/Stack.hpp
@@ -1,111 +1,105 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
-#include <iostream>
-#include <string>
-#include <stdexcept>
+#include <iostream> 
+#include <string> 
+#include <stdexcept> 
+
 using namespace std;
 
 template <typename T>
 class Stack {
-    T * array_; 
-    size_t array_size_;
-    size_t count_ = 0;
+private:
+	T* array_;
+	size_t array_size_;
+	size_t count_;
 public:
-   
-    Stack() noexcept: array_size_(size_t(10)),
-        array_(new T[10]) { //Конструктор для стека по умолчанию
-    };
-   
-    Stack(size_t st) noexcept: array_size_(st),
-        array_(new T[st]) { //Конструктор для стека с параметром
-    };
-    
-    Stack(const Stack& s) noexcept{
-        delete[] array_;
-        array_size_ = s.array_size_;
-        count_ = s.count_;
-        array_ = new T[s.array_size_];
-        for(size_t i = 0; i < s.count_; ++i) {
-          array_[i] = s.array_[i];
-        }
-    };
-   
-    Stack(Stack&& s) noexcept{
-      array_size_ = s.array_size_;
-      count_ = s.count_;
-      array_ = s.array_;
-      s.array_size_ = 0;
-      s.count_ = 0;
-      s.array_ = nullptr;
-    };
-   
-    Stack<T>& operator=(const Stack& s) noexcept{
-      if(this != &s) {
-        delete[] array_;
-        array_size_ = s.array_size_;
-        count_ = s.count_;
-        array_ = new T[s.array_size_];
-        for(size_t i = 0; i < s.count_; ++i) {
-          array_[i] = s.array_[i];
-        }
+	Stack() noexcept : array_size_(8), array_(new T[array_size_]), count_(0) {}
+	Stack(size_t max_el) noexcept : array_size_(max_el), array_(new T[max_el]), count_(0) {}
+
+	Stack(const Stack& s) noexcept {
+		if (this != &s) {
+			delete[] array_;
+			array_size_ = s.array_size_;
+			count_ = s.count_;
+			array_ = new T[s.array_size_];
+			for (size_t i = 0; i < s.array_size_; ++i) {
+				array_[i] = s.array_[i];
+			}
+		}
+	}
+
+	Stack(Stack&& s) noexcept : array_size_(s.array_size_), count_(s.count_), array_(s.array_) {
+
+		s.array_size_ = 0;
+		s.count_ = 0;
+		s.array_ = nullptr;
+	}
+
+	Stack<T>& operator=(const Stack& s) noexcept{
+      		if(this == &s) return *this;
+      		else {
+        		delete[] array_;
+        		array_size_ = s.array_size_;
+        		count_ = s.count_;
+        		array_ = new T[s.array_size_];
+        		for(size_t i = 0; i < s.array_size_; ++i) {
+         	 array_[i] = s.array_[i];
+        	}
+        	return *this;
       }
-      return *this;
-    };
-    
-    Stack<T>& operator=(Stack&& s) noexcept{
-      if(this != &s) {
-        array_size_ = s.array_size_;
-        count_ = s.count_;
-        array_ = s.array_;
-        s.array_size_ = 0;
-        s.count_ = 0;
-        s.array_ = nullptr;
-      }
-      return *this;
-    };
-
-    T& operator [](int pos) noexcept {
-        return array_[pos];
-    };
-    //Метод count
-    size_t count() const noexcept{
-        return count_;
-    };
-
-    void push(T const &value) noexcept{
-        if (count_ == array_size_) {
-            T* new_array_ = new T[2*array_size_];
-            for (int i = 0; i < array_size_; ++i) {
-                new_array_[i] = array_[i]; 
-            }
-            delete[] array_; 
-            array_ = new_array_; 
-        }
-        array_[count_] = value; 
-        ++count_; 
-    };
-
-    T top() const noexcept{
-      return array_[count_];
     }
-
-    void pop() noexcept{
-        if (count_ == 0) {
-            throw runtime_error("---Stack is empty---");
-        }
-        --count_;
-    };
-
-    bool empty() const noexcept{
-        if(count() == 0) return true;
-        return false;
-    };
   
-    ~Stack<T>() noexcept{
-        delete[] array_;
-    };
+    	Stack<T>& operator=(Stack&& s) noexcept{
+      		array_size_ = s.array_size_;
+      		count_ = s.count_;
+      		array_ = s.array_;
+      		s.array_size_ = 0;
+      		s.count_ = 0;
+      		s.array_ = nullptr;
+      		return *this;
+    }
+	size_t count() const noexcept {
+		return count_;
+	}
+
+	bool empty() const noexcept {
+		if (count() == 0) return true;
+		return false;
+	}
+
+	void push(T const &value) noexcept {
+		if (count_ == array_size_) {
+			T* array_size2_ = new T[2 * array_size_];
+			for (int i = 0; i != array_size_; ++i) {
+				array_size2_[i] = array_[i];
+			}
+			array_size_ *= 2;
+			delete[] array_;
+			array_ = array_size2_;
+		}
+		array_[count_++] = value;
+	}
+
+	T& top() const noexcept {
+		return array_[count_];
+	}
+
+	T& pop() noexcept {
+		if (count_ == 0) domain_error{ "ERROR —- STACK EMPTY" };
+	--count_;
+		return top();
+	}
+
+	~Stack() noexcept {
+		delete[] array_;
+	}
+
+	T& operator[](int ij) noexcept {
+		return array_[ij];
+	}
 };
+
 
 
 #endif
