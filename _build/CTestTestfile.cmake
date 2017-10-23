243fde8fe253180c0d962567d22155f2410736d7
ZolbergN <exculibur@rambler.ru>
2017-10-23 12:37:33 +0300

    remove build

--- _build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/123/Stack_04_tests
-# Build directory: /home/123/Stack_04_tests/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/123/Stack_04_tests/_build/check.exe" "-s" "-r" "compact" "--use-colour" "yes")
